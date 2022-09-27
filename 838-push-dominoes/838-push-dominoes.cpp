69cf519247f2d7179acf9e3286f0d34c64a5faf4
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2022-09-27 17:45:27 +0530

    Time: 23 ms (99.65%), Space: 12.5 MB (97.92%) - LeetHub

---
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string pushDominoes(string dominoes) {
+        int n = dominoes.size();
+        int right = -1;
+        for(int i = 0; i<n; i++){
+            if(dominoes[i] == 'L'){
+                if(right == -1){
+                    for(int j = i-1; j>=0 and dominoes[j] == '.'; j--){
+                        dominoes[j] = 'L';
+                    }
+                }
+                else{
+                    for(int j = right+1, k = i-1; j<k; j++,k--){
+                        dominoes[j] = 'R';
+                        dominoes[k] = 'L';
+                    }
+                    right = -1;
+                }
+            }
+            else if(dominoes[i] == 'R'){
+                if(right != -1){
+                    for(int j = right+1; j<i; j++){
+                        dominoes[j] = 'R';
+                    }
+                }
+                right = i;
+            }
+        }
+        if(right != -1){
+            for(int j = right+1; j<n; j++){
+                dominoes[j] = 'R';
+            }
+        }
+        return dominoes;
+    }
+};
