61f575374e4bcbd5a0d50c98fdc316100715f96e
Gantavya Malviya <39916680+gantavyamalviya@users.noreply.github.com>
2022-10-07 00:04:20 +0530

    Time: 47 ms (98.16%), Space: 9.5 MB (96.71%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -3,9 +3,13 @@ public:
     bool canPartition(vector<int>& nums) {
         int totalSum = accumulate(begin(nums), end(nums), 0), halfSum = totalSum / 2;
         if(totalSum & 1) return false;
-        bitset<10001> dp(1);       // 0 is always achievable => dp[0] = 1
+        bool dp[halfSum+1]; memset(dp, false, sizeof dp);
+        dp[0] = true;                              // 0 sum is always achievable
         for(int num : nums) 
-            dp = dp | dp << num;
+            for(int j = halfSum; j >= num; j--)    // essential to start right to left
+                if(dp[j - num])                    // if j - num was previously achievable
+                    dp[j] = true;                  // we can add num to it and make j achievable as well
+            
         return dp[halfSum];
     }
 };
